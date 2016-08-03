2a2c2cb72e0e52faa21071cedf58da84249a1081
Philip Neustrom <philipn@gmail.com>
2016-08-02 20:17:54 -0700

    v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='k8env',
-    version='0.1.0',
+    version='0.1.1',
     url='http://github.com/philipn/k8env',
     license='MIT',
     description='A simple way to manage different Kubernetes contexts',
